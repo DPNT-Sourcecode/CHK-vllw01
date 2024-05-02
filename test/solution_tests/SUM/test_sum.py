637e4ae2dd25503224a3252ba48b0351abea260f
sbx_user1051 <sbx_user1051@169.254.61.37>
2024-05-02 14:08:53 +0000

    Thu May 02 14:08:53 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,7 @@
-from solutions.SUM import sum_solution
+from accelerate_runner.solutions.SUM import sum_solution
 
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
